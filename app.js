6db8bc7fea167048f504bb8419ad1deff7fcdfa6
Shankara21 <lazuardit21@gmail.com>
2023-02-19 21:39:47 +0700

    update cors , etc

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Headers", "*");
   next();
 });
-app.use(cors({ origin: "http://localhost:3001", credentials: true }));
+app.use(cors({ origin: true, credentials: true }));
 app.use(cookieParser());
 
 app.use(logger("dev"));
