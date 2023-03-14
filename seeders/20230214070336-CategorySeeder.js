1e0cab2737a401a50979c0cf8491227185d4f2bb
Shankara21 <lazuardit21@gmail.com>
2023-03-14 08:19:35 +0700

    update category

--- seeders/20230214070336-CategorySeeder.js
+++ seeders/20230214070336-CategorySeeder.js
@@ -1,33 +1,37 @@
-'use strict';
+"use strict";
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    await queryInterface.bulkInsert('MstCategories', [
-      {
-        name: 'ISP',
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-      {
-        name: 'WI',
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-      {
-        name: 'FORM',
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-      {
-        name: 'Others',
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-    ], {});
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert(
+      "MstCategories",
+      [
+        {
+          name: "ISP",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          name: "WI",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          name: "FORM",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          name: "Others",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
   },
 
-  async down (queryInterface, Sequelize) {
-    await queryInterface.bulkDelete('MstCategories', null, {});
-  }
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("MstCategories", null, {});
+  },
 };
